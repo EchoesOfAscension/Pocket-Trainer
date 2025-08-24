fb501b0a2a2ff3d83a597591924990179f11975a
EchoesOfAscension <EchoesOfAscension@users.noreply.github.com>
2025-08-24 22:23:38 +0000

    Unpacked site zip

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy({ "static": "media" });
+  return {
+    dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["njk", "md", "html"]
+  };
+};
